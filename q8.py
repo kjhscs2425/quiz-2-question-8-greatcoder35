270bec82728375a9d8110c58955ac74fd3ea8953
Jordan Nash <jordangabrielnash@gmail.com>
2025-02-20 11:09:23 -0800

    quiz done

--- q8.py
+++ q8.py
@@ -8,3 +8,14 @@ ends with one of these letters: p, t, n
 """
 
 # YOUR CODE HERE
+start_letter = ["c","t","b"]
+middle_letter = ["a","o"]
+end_letter = ["p","t","n"]
+word_count = 1
+
+for first in start_letter:
+    for middle in middle_letter:
+        for last in end_letter:
+            word = first + middle + last
+            print(f"{word_count}: {word}")
+            word_count += 1
